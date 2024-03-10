8bd78cd942b094ff5b6f74152467f1d1a65c46ab
nilskrannig <nils.krannig@gmx.de>
2024-03-10 19:53:31 +0100

    Part3_Exercises: Phonebook database Step 5

--- index.js
+++ index.js
@@ -53,6 +53,12 @@ app.post('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response) => {
+  Person.findByIdAndUpdate(request.params.id, request.body)
+    .then(updatedPerson => response.json(updatedPerson))
+    .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
   console.log(error.message);
 
