b363bf01f8f1c9092ce7df112450fd86687f1135
nilskrannig <nils.krannig@gmx.de>
2024-03-11 14:35:24 +0100

    Part3_Exercises: Phonebook database Step 8

--- models/person.js
+++ models/person.js
@@ -16,13 +16,24 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: Number,
+    number: {
+        type: String,
+        minLength: 8,
+        validate:
+        {
+            validator: (v) => {
+                return /^\d{2,3}-\d+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number`
+        },
+        required: [true, 'Phone number required']
+    },
 })
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString(),
-        delete returnedObject._id
+            delete returnedObject._id
         delete returnedObject.__v
     }
 })
